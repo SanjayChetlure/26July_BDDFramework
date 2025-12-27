98a4014158d56a25d206976d73cfe191d6a24f4d
SanjayChetlure <sanjay.chetlure@gmail.com>
2025-12-27 09:36:39 +0530

    new file demo added

--- Features/REx1_LoginToSwagLabApp.feature
+++ Features/REx1_LoginToSwagLabApp.feature
@@ -8,12 +8,12 @@ Background:
     When user enter username "UN" on swagLab login page
     And user enter password "PWD" on swagLab login page
     And user clicks login btn on swagLab login page
-    Then user should be on swagLab home page with logoText "Swag Labs"
+    Then user should be on swagLab home page with logoText "Swag Labs1"
 
-  @sanity
-  Scenario: s2- login to swaglab app using invalid credentials
-    When user enter username "UN" on swagLab login page
-    And user enter password "WrongPWD" on swagLab login page
-    And user clicks login btn on swagLab login page
-    Then error message is visible
+#  @sanity
+#  Scenario: s2- login to swaglab app using invalid credentials
+#    When user enter username "UN" on swagLab login page
+#    And user enter password "WrongPWD" on swagLab login page
+#    And user clicks login btn on swagLab login page
+#    Then error message is visible
 
--- Features/environment.py
+++ Features/environment.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import allure
 from selenium import webdriver
 
 from Utilities import ConfigReader
@@ -14,7 +17,10 @@ from Utilities import ConfigReader
 def before_scenario(context, scenario):
     print("Starting test execution: opening browser")
 
-    browserName=ConfigReader.read_configuration("basic info","browser")
+    defaultBrowserName=ConfigReader.read_configuration("basic info","browser")
+
+    # Get browser from CLI parameter, default browser name get it from config.ini
+    browserName = context.config.userdata.get("browser", defaultBrowserName).lower()
 
     if browserName=="chrome":
         context.driver = webdriver.Chrome()
@@ -28,12 +34,34 @@ def before_scenario(context, scenario):
 
 def after_scenario(context, scenario):
     print("Starting test execution: opening browser")
+    if scenario.status == "failed":
+        scenario_name = scenario.name.replace(" ", "_").replace('"', "").replace("'", "")   # Clean scenario name to avoid invalid file characters
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")                              # get current date time Timestamp
+        file_path = f"screenshots/Failed_{scenario_name}_{timestamp}.png"                    # Final filename format
+        context.driver.save_screenshot(file_path)
+
+        # Attach screenshot to the Allure report
+        with open(file_path, "rb") as f:
+            allure.attach(
+                f.read(),
+                name=file_path,
+                attachment_type=allure.attachment_type.PNG
+            )
+
+
+
     context.driver.quit()
 
 
-def after_step(context, step):
-    if step.status == "failed":
-        context.driver.save_screenshot(f"screenshots/{step.name}.png")
+
+# def after_step(context, step):
+#     if step.status == "failed":
+#         safe_name = step.name.replace(" ", "_").replace('"', "").replace("'", "")
+#         context.driver.save_screenshot(f"screenshots/{safe_name}.png")
+
+# def after_step(context, step):
+#     step_name = step.name.replace(" ", "_").replace('"', "").replace("'", "")
+#     context.driver.save_screenshot(f"screenshots/{step_name}.png")
 
 # def before_step(context, step):
 #     print(f"Starting step:")
